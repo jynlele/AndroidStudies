f93a0195bcffc85e551bb22b8800ebdefcd63e83
jynlele <jynlele@gmail.com>
2018-02-06 15:42:42 -0500

    try to show the list

--- app/src/main/java/com/example/bugles/listdemo/ListViewInListActivity.java
+++ app/src/main/java/com/example/bugles/listdemo/ListViewInListActivity.java
@@ -2,16 +2,21 @@ package com.example.bugles.listdemo;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
-public class ListViewInListActivity extends ListActivity {
-    private static final String[] names = new String[]{"斗破苍穹","网游之终极牧师","琴帝"};
+public class ListViewInListActivity extends AppCompatActivity {
+    private static final String[] names = new String[]{"斗破苍穹", "网游之终极牧师", "琴帝"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.abc_action_bar_view_list_nav_layout);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout. abc_list_menu_item_layout, names);
-        setListAdapter(adapter);
-    }
+        setContentView(R.layout.activity_main);
 
+        Log.d("debug","To the second Activity");
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, names);
+//        setListAdapter(adapter);
+    }
 }
+
--- app/src/main/java/com/example/bugles/listdemo/MainActivity.java
+++ app/src/main/java/com/example/bugles/listdemo/MainActivity.java
@@ -6,20 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button bbb;
-    private TextView tv;
-    private View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(MainActivity.this, ListViewInListActivity.class);
-            startActivity(intent);
-        }
-    };
+
 
     private Handler handler = new Handler();
 
@@ -27,20 +21,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent it = new Intent(MainActivity.this, ListViewInListActivity.class);
+        startActivity(it);
 
-        bbb.setOnClickListener(listener);
+
 
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 final String result = MineUtil.get("https://news-app.apidev.51.ca/get_yellowpages_list?category=46&offset=0&limit=20");
+                final String[] res = {result, "haha", "lala"};
 
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
                         Log.d("debug", result);
                         Log.d("debug", "I got here!!!");
+                        showMyStuff(res);
                     }
                 });
             }
@@ -48,6 +46,18 @@ public class MainActivity extends AppCompatActivity {
             }).start();
 
 
+
+    }
+
+    private void showMyStuff (String[] str){
+        //build Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.item,
+                str);
+        //Configure the list view
+        ListView list = (ListView) findViewById(R.id.listViewMain);
+        list.setAdapter(adapter);
 
     }
 }
