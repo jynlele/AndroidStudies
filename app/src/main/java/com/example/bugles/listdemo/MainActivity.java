9c7c6d9895cf182ea32bda39aa53d91bdae8f683
jynlele <jynlele@gmail.com>
2018-02-08 16:07:31 -0500

    List is showisng now and try to add refress action

--- app/src/main/java/com/example/bugles/listdemo/MainActivity.java
+++ app/src/main/java/com/example/bugles/listdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bugles.listdemo;
 
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,8 +22,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent it = new Intent(MainActivity.this, ListViewInListActivity.class);
-        startActivity(it);
+        /*
+        to test new Activity avaliable
+         */
+//        Intent it = new Intent(MainActivity.this, ListViewInListActivity.class);
+//        startActivity(it);
+//
+//        String[] ln = {"a", "b", "c"};
+//        showMyStuff(ln);
+
+
+//        final SwipeRefreshLayout srl = (SwipeRefreshLayout) findViewById(R.id.listViewMain);
+//        srl.setColorSchemeColors(R.color.refresh, R.color.refresh1, R.color.refresh2);
+//        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//                srl.setRefreshing(true);
+//                (new Handler()).postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        srl.setRefreshing(false);
+//
+//
+//                    }
+//                },3000);
+//            }
+//        });
 
 
 
@@ -39,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("debug", result);
                         Log.d("debug", "I got here!!!");
                         showMyStuff(res);
+
                     }
                 });
             }
@@ -50,14 +76,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showMyStuff (String[] str){
+
+
         //build Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
-                R.layout.item,
+                R.layout.a_item,
                 str);
         //Configure the list view
         ListView list = (ListView) findViewById(R.id.listViewMain);
+
         list.setAdapter(adapter);
+        Log.d("debug", "showMyStuff is running");
 
     }
 }
